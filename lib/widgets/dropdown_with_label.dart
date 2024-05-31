40f99e4700c274c6cbcaade638526e7b187721a7
Leo Tschritter <leotschritter@gmail.com>
2024-05-31 14:23:53 +0200

    widgets: Created Label to combine Label and Dropdown Widget

---
+++ lib/widgets/dropdown_with_label.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class DropdownWithLabel extends StatelessWidget {
+  final Widget dropdown;
+  final String label;
+
+  const DropdownWithLabel({super.key, required this.dropdown, required this.label});
+
+  @override
+  Widget build(final BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          label,
+          style: TextStyle(
+            color: theme.colorScheme.onPrimary,
+            fontSize: 14,
+          ),
+        ),
+        const SizedBox(height: 8,),
+        dropdown,
+      ],
+    );
+  }
+}
