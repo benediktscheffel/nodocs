567ef0365762fe8947c8468765bac8003692fc28
Leo Tschritter <leotschritter@gmail.com>
2024-07-03 20:52:54 +0200

    remove unused imports

--- lib/features/scan/controller/scan_controller.dart
+++ lib/features/scan/controller/scan_controller.dart
@@ -1,6 +1,3 @@
-import 'package:camera/camera.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:nodocs/features/scan/services/image_service.dart';
 
 abstract class ScanController {
   List<String> addToImagePaths(final List<String> imagePaths, final String path);
