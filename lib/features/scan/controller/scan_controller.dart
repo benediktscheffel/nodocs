ca20d3d70af1bfa1e63d97ef75b2579dde0bf7f5
benediktscheffel <benedikt.scheffel@hotmail.de>
2024-07-04 19:52:15 +0200

    feature-38: fixing analyzer warnings

--- lib/features/scan/controller/scan_controller.dart
+++ lib/features/scan/controller/scan_controller.dart
@@ -1,13 +1,14 @@
-import 'package:camera/camera.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:nodocs/features/scan/services/image_service.dart';
-
 abstract class ScanController {
-  List<String> addToImagePaths(final List<String> imagePaths, final String path);
+  List<String> addToImagePaths(
+      final List<String> imagePaths, final String path);
+
   List<String> removeFromImagePaths(final List<String> imagePaths);
+
   String getLatestImagePath(final List<String> imagePaths);
+
   int getScanCounter(final List<String> imagePaths);
 
   void goToPage(final Uri uri);
+
   void goBack();
 }
